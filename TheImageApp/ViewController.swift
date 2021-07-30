e133fca01ab9b98540649ef4682f501675398b01
trinhphu22 <87976330+trinhphu22@users.noreply.github.com>
2021-07-30 18:11:37 +0700

    Initial Commit

---
+++ TheImageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheImageApp
+//
+//  Created by Forly on 30/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
